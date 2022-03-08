90ea1f3124703adb8ca675b071f16d2290ff6d2c
paoxiaode <1435709051@qq.com>
2022-03-08 22:28:10 +0800

    3.8

--- ch10_generic_algorithm/exercise.cpp
+++ ch10_generic_algorithm/exercise.cpp
@@ -5,6 +5,16 @@
 #include<algorithm>
 using namespace std;
 
+void elimDups(vector<string>& words){
+    sort(words.begin(), words.end());
+    auto end_unique = unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+bool stringlarger(const string& s){
+    return s.size()>3;
+}
+
 int main(){
     vector<int> vec{1,3,4,5};
 
@@ -16,4 +26,18 @@ int main(){
     fill_n(vec.begin(), vec.size(), 0);
     for(auto &i: vec)cout << i << " ";
     cout << endl;
+
+    //10.9
+    vector<string> vec_string{"fox", "jumps", "over", "quick", "red", "red", "the", "the"};
+    elimDups(vec_string);
+    for(auto i:vec_string) cout << i << " ";
+    cout << endl;
+
+    //10.13
+    auto iter = partition(vec_string.begin(), vec_string.end(), stringlarger);
+    while(--iter != vec_string.begin() - 1){
+        cout << *iter << " ";
+    }
+    cout << endl;
+
 }
